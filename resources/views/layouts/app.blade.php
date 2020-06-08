4c8df4708aece13508f8029f18842ba9682104c0
unknown <saaddae003@st.ug.edu.gh>
2020-06-08 12:03:11 +0000

    updating link file in app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,15 +10,15 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ secure_asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet" media="print">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet" media="all">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet" media="print">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet" media="all">
 </head>
 <body>
     <div id="app">
